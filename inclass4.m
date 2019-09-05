f6d58b0334e870560ecddf29e454e157f65546d6
warmflasha <warmflasha@gmail.com>
2019-09-05 11:38:18 -0500

    Added inclass4

---
+++ inclass4.m
@@ -0,0 +1,27 @@
+%Note: when working with functions for homeworks: please put the functions
+%as separate files in the repository. Use the main homework file to call
+%the functions and demonstrate that they work. Make sure the functions are
+%appropriately commented (so that "help {function name}" produces
+%something useful. 
+
+% 1. Write a function that takes an array as input and returns a logical
+% variable which is true if the sum of the numbers in the array is greater
+% than or equal to 10 and false if it is less than 10. 
+
+%2. Write a function to take the reverse complement of a DNA sequence, that
+%is, returns the complementary base pair for each base, read from end
+%to beginning. (e.g. the reverse compliment of ATGC is GCAT). Do not use the
+%builtin MATLAB function for this. 
+
+%3. The fibonacci sequence is defined such that each number in the sequence
+%after the first two is the sum of the previous 2. For example:
+% 1, 1, 2, 3, 5, 8, 13, 21....
+% (a) Write a function that takes two inputs - an array containing two integers
+% which will be the first two values of a fibonacci sequence and an integer
+% N which is the number of values desired and returns a fibonacci sequences
+% of length N with the specified first two numbers. (b) Write a function which 
+% takes a fibonacci sequence as input and produces the next number of that
+% sequence as output. (c)Write a function with same input-output
+% relationship as in (a) but that uses your function from (b) to do the
+% computation. 
+
